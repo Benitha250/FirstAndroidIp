ddadce070ca9c739e7f751d7a78395570e5a1c4a
Benitha250 <benithauwase05@gmail.com>
2021-01-07 10:07:47 +0200

    add an activity where to see the list of clothes

---
+++ app/src/main/java/com/example/firstandroidip/ListDesignActivity.java
@@ -0,0 +1,14 @@
+package com.example.firstandroidip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListDesignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_design);
+    }
+}
